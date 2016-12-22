e4955850f67dc2e4bd7fcaa6371125637f5af47a
Koala Yeung <koalay@gmail.com>
2016-12-22 12:27:44 +0800

    httpcache: A simple memcache cacher
    Implemented a simple http.Handler based middleware to cache
    HTTP response. The cache mechanism is controlled by header
    `Expires` field and `X-Grace-Expires` field.

---
+++ httpcache/cache.go
@@ -0,0 +1,239 @@
+package httpcache
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/bradfitz/gomemcache/memcache"
+)
+
+const fmtRFC2612 = "Mon, 02 Jan 2006 15:04:05 GMT"
+
+func parseRFC2612(str string) (t time.Time, err error) {
+	if len(str) < 3 {
+		err = fmt.Errorf("incorrect time string provided: %s", str)
+		return
+	}
+	t, err = time.Parse(fmtRFC2612, str[:len(str)-3]+"GMT")
+	if err != nil {
+		// undo the string replacenemt for better error messages
+		terr := err.(*time.ParseError)
+		err = fmt.Errorf("cannot parse time %#v as RFC2612 (%#v)", str, terr.Layout)
+	}
+	return
+}
+
+var client *memcache.Client
+
+func init() {
+	memcachedURL := strings.Split(os.Getenv("MEMCACHED_URL"), ";")
+	client = memcache.New(memcachedURL...)
+}
+
+// NewCache wraps an http.ResponswWriter with Cache
+func NewCache(w http.ResponseWriter) *Cache {
+	return &Cache{
+		responseWriter: w,
+		Created:        time.Now(),
+		Status:         http.StatusOK,
+		content:        bytes.NewBuffer(make([]byte, 0, 4096)), // pre=alloc 4096 bytes for buffer
+	}
+}
+
+// Cache wraps an http.ResponseWriter and return
+type Cache struct {
+	responseWriter http.ResponseWriter
+	content        *bytes.Buffer
+	Created        time.Time
+	Status         int
+	CachedHeader   http.Header // header loaded from cache
+	CachedContent  string
+}
+
+// Code returns the cached http status code
+func (cache *Cache) Code() int {
+	return cache.Status // TODO; ensure the default value is http.StatusOK
+}
+
+// TODO: add method to check Last-Modified date / Date of the cached response
+
+// Expired check if the cache should expire
+func (cache *Cache) Expired() bool {
+	var expires time.Time
+	var err error
+
+	// TODO: might support max-age somehow?
+
+	// parse grace expires override
+	if expiresStr := cache.Header().Get("X-Grace-Expires"); expiresStr != "" {
+		expires, err = parseRFC2612(expiresStr)
+		if err != nil {
+			log.Printf("error parsing X-Grace-Expires: %s", err.Error())
+			return true // treat as expired if error
+		}
+	} else if expiresStr := cache.Header().Get("Expires"); expiresStr != "" {
+		expires, err = parseRFC2612(expiresStr)
+		if err != nil {
+			log.Printf("error parsing Expires: %s", err.Error())
+			return true // treat as expired if error
+		}
+	}
+
+	if time.Now().Before(expires) {
+		return false
+	}
+
+	return true // default treat as expired
+}
+
+// String return buffered content as string
+func (cache *Cache) String() string {
+	if cache.responseWriter != nil {
+		return cache.content.String()
+	}
+	return cache.CachedContent
+}
+
+// Bytes return buffered content as string
+func (cache *Cache) Bytes() []byte {
+	if cache.responseWriter != nil {
+		return cache.content.Bytes()
+	}
+	return []byte(cache.CachedContent)
+}
+
+// WriteTo writes the content of current cache to http.ResponseWriter
+func (cache *Cache) WriteTo(w http.ResponseWriter) {
+	header := cache.Header()
+	for name := range header {
+		for i := range header[name] {
+			w.Header().Add(name, header[name][i])
+		}
+	}
+	w.WriteHeader(cache.Code())
+	w.Write(cache.Bytes())
+}
+
+// Header implements http.ResponseWriter
+func (cache *Cache) Header() http.Header {
+	if cache.responseWriter != nil {
+		return cache.responseWriter.Header()
+	}
+	return cache.CachedHeader
+}
+
+// Write implements http.ResponseWriter
+func (cache *Cache) Write(p []byte) (int, error) {
+	cache.content.Write(p) // omit write number and error in buffer write
+	return cache.responseWriter.Write(p)
+}
+
+// WriteHeader implements http.ResponseWriter
+func (cache *Cache) WriteHeader(code int) {
+	cache.Status = code
+	cache.responseWriter.WriteHeader(code)
+}
+
+func keyOf(r *http.Request) (key string, err error) {
+	if r == nil {
+		err = fmt.Errorf("request cannot be nil")
+		return
+	}
+	if r.URL == nil {
+		err = fmt.Errorf("r.URL cannot be nil")
+		return
+	}
+	key = "page:/" + r.URL.Path
+	return
+}
+
+// Load cache for a given http request
+func Load(r *http.Request) (cache *Cache, err error) {
+	key, err := keyOf(r)
+	if err != nil {
+		return
+	}
+	p, err := client.Get(key)
+	if err != nil {
+		return
+	}
+
+	cache = &Cache{}
+	if err = json.Unmarshal(p.Value, cache); err != nil {
+		cache = nil
+		return
+	}
+	return
+}
+
+// Save cache for a given http request
+func Save(r *http.Request, cache *Cache) (err error) {
+	key, err := keyOf(r)
+	if err != nil {
+		return
+	}
+
+	// ensure that the header is cached
+	cache.CachedHeader = cache.Header()
+	cache.CachedContent = cache.String()
+
+	p, err := json.Marshal(cache)
+	if err != nil {
+		return
+	}
+
+	//log.Printf("save cache: %s, %s", key, p)
+
+	// store the httpcache item in memcached
+	client.Set(&memcache.Item{
+		Key:   key,
+		Value: p,
+	})
+	return nil
+}
+
+// Delete deletes cache of a given request
+func Delete(r *http.Request) (err error) {
+	key, err := keyOf(r)
+	if err != nil {
+		return
+	}
+	return client.Delete(key)
+}
+
+// CacheHandler applies httpcache to the wrapped http.Handler
+func CacheHandler(inner http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		requestID := r.Header.Get("X-Request-ID")
+
+		// try to load cache for the request
+		cache, err := Load(r)
+		if err != nil {
+			log.Printf("request-id=%s, message=error loading cache: %s", requestID, err.Error())
+		}
+
+		// if has cache, write to ResponseWriter and return early
+		if cache != nil && !cache.Expired() {
+			cache.WriteTo(w)
+			return // early return
+		}
+
+		// refresh cache by running inner handler
+		log.Printf("request-id=%s, message=no valid cache, trigger inner handler", requestID)
+		cache = NewCache(w)
+		inner.ServeHTTP(cache, r)
+		go func() {
+			err := Save(r, cache)
+			if err != nil {
+				log.Printf("request-id=%s, message=error saving cache: %s", requestID, err.Error())
+			}
+		}()
+	})
+}
---
+++ httpcache/cache_test.go
@@ -0,0 +1,199 @@
+package httpcache_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/yookoala/weatherhk/httpcache"
+)
+
+const fmtRFC2612 = "Mon, 02 Jan 2006 15:04:05 GMT"
+
+func rfc2616(t time.Time) string {
+	// RFC2616: Tue, 15 Nov 1994 12:45:26 GMT
+	// RFC1123: Mon, 02 Jan 2006 15:04:05 MST
+	return t.In(time.UTC).Format(fmtRFC2612)
+}
+
+func TestCache_implementsResponseWriter(t *testing.T) {
+	var cache http.ResponseWriter = &httpcache.Cache{}
+	_ = cache // just to verify httpcache.Cache implements http.ResponseWriter
+}
+
+func TestCache(t *testing.T) {
+
+	// handler to be wrapped
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Custom message")
+		w.Header().Add("X-Special-Header", "some special header message")
+		w.Header().Add("Expires", rfc2616(time.Now().Add(-60*time.Second))) // should be rfc2616 format
+		w.WriteHeader(http.StatusPartialContent)
+	})
+
+	w := httpcache.NewCache(httptest.NewRecorder())
+	handler.ServeHTTP(w, nil)
+
+	if want, have := "Custom message", w.String(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+	if want, have := "Custom message", string(w.Bytes()); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+	if want, have := "some special header message", w.Header().Get("X-Special-Header"); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+	if want, have := http.StatusPartialContent, w.Code(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+	if want, have := true, w.Expired(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+}
+
+func TestCache_Expired(t *testing.T) {
+	w := httpcache.NewCache(httptest.NewRecorder())
+
+	// manually update "Expires" header and check again
+	w.Header().Set("Expires", rfc2616(time.Now().Add(60*time.Second))) // should be rfc2616 format
+	if want, have := false, w.Expired(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+
+	w.Header().Set("Expires", rfc2616(time.Now().Add(-60*time.Second))) // should be rfc2616 format
+	if want, have := true, w.Expired(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+
+	// way to pass grace time instruction without
+	// polluting "Expires" header (custom "X-Grace-Expires" header)
+	w.Header().Set("Expires", rfc2616(time.Now().Add(-60*time.Second)))        // should be rfc2616 format
+	w.Header().Set("X-Grace-Expires", rfc2616(time.Now().Add(60*time.Second))) // should be rfc2616 format
+	if want, have := false, w.Expired(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+
+	w.Header().Del("X-Grace-Expires")
+	w.Header().Set("Expires", "some non-sense")
+	if want, have := true, w.Expired(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+
+	w.Header().Del("Expires")
+	w.Header().Set("X-Grace-Expires", "some non-sense")
+	if want, have := true, w.Expired(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+
+	w.Header().Del("Expires")
+	w.Header().Del("X-Grace-Expires")
+	if want, have := true, w.Expired(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+
+}
+
+func TestCache_WriteTo(t *testing.T) {
+	w1 := httpcache.NewCache(httptest.NewRecorder())
+	w1.Header().Add("X-Custom-Header", "value 1")
+	w1.Header().Add("X-Custom-Header", "value 2")
+	w1.Header().Add("X-Custom-Header", "value 3")
+	w1.WriteHeader(http.StatusBadGateway)
+	w1.Write([]byte("Hello content"))
+
+	w2 := httptest.NewRecorder()
+	w1.WriteTo(w2)
+	if want, have := len(w2.Header()), len(w1.Header()); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+		return
+	}
+	if want, have := len(w2.Header()["X-Custom-Header"]), len(w1.Header()["X-Custom-Header"]); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+	if want, have := w2.Header()["X-Custom-Header"][0], w1.Header()["X-Custom-Header"][0]; want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+	if want, have := w2.Header()["X-Custom-Header"][1], w1.Header()["X-Custom-Header"][1]; want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+	if want, have := w2.Header()["X-Custom-Header"][2], w1.Header()["X-Custom-Header"][2]; want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+}
+
+func TestLoad(t *testing.T) {
+	_, err := httpcache.Load(nil)
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+
+	r, _ := http.NewRequest("GET", "", nil)
+	r.URL = nil
+	_, err = httpcache.Load(r)
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
+
+func TestCacheHandler(t *testing.T) {
+
+	if url := os.Getenv("MEMCACHED_URL"); url == "" {
+		t.Skip("MEMCACHED_URL not set, test skipped")
+	}
+
+	// handler to be wrapped
+	calledHandler := 0
+	var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Custom message")
+		w.Header().Add("X-Special-Header", "some special header message")
+		w.Header().Add("Expires", rfc2616(time.Now().Add(60*time.Second)))
+		w.WriteHeader(http.StatusPartialContent)
+		calledHandler += 1
+	})
+
+	// serve request for handler
+	handler = httpcache.CacheHandler(handler)
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/some/path.html", nil)
+	handler.ServeHTTP(w, r)
+
+	if want, have := 1, calledHandler; want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+
+	// wait a bit for memcached to handle
+	time.Sleep(10 * time.Millisecond)
+
+	// check if the cache actually works
+	handler.ServeHTTP(w, r)
+	if want, have := 1, calledHandler; want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+
+	// examine the cached content
+	cache, err := httpcache.Load(r)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err.Error())
+	}
+	if cache == nil {
+		t.Error("cache is nil")
+		return
+	}
+	defer httpcache.Delete(r)
+
+	if want, have := "Custom message", cache.String(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+	if want, have := "Custom message", string(cache.Bytes()); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+	if want, have := "some special header message", cache.Header().Get("X-Special-Header"); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+	if want, have := http.StatusPartialContent, cache.Code(); want != have {
+		t.Errorf("expected %#v, got %#v", want, have)
+	}
+}
