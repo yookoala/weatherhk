3569c7e70aa1508299c8e2dd41d6ffb97e17fe09
Koala Yeung <koalay@gmail.com>
2019-02-02 13:34:50 +0800

    hkodata: fix test print out
    * fix printf substitution mistake.

--- hkodata/current_weather_test.go
+++ hkodata/current_weather_test.go
@@ -34,78 +34,78 @@ func TestCurrentWeather(t *testing.T) {
 		t.Errorf("expected %#v, got %#v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.HongKongObservatory; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(17), cw.DistrictsTemperature.KingsPark; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.WongChukHang; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(16), cw.DistrictsTemperature.TaKwuLing; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(17), cw.DistrictsTemperature.LauFauShan; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(17), cw.DistrictsTemperature.TaiPo; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.ShaTin; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.TuenMun; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(17), cw.DistrictsTemperature.TseungKwanO; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.SaiKung; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(17), cw.DistrictsTemperature.CheungChau; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(19), cw.DistrictsTemperature.ChekLapKok; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.ShekKong; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(16), cw.DistrictsTemperature.TsuenWanHoKoon; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(17), cw.DistrictsTemperature.TsuenWanShingMunValley; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.HongKongPark; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.ShauKeiWan; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(17), cw.DistrictsTemperature.KowloonCity; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(19), cw.DistrictsTemperature.HappyValley; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.WongTaiSin; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.Stanley; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.KwunTong; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.ShamShuiPo; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(19), cw.DistrictsTemperature.KaiTakRunwayPark; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 	if want, have := hkodata.Temperature(18), cw.DistrictsTemperature.YuenLongPark; want != have {
-		t.Errorf("expected %d, got %d", want, have)
+		t.Errorf("expected %v, got %v", want, have)
 	}
 }
